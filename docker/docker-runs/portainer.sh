df6dc3f505b062c94779feab12da2337b73ec255
mastermindzh <Rick@mi-soft.nl>
2017-05-10 23:49:04 +0200

    fixed portainer

--- docker/docker-runs/portainer.sh
+++ docker/docker-runs/portainer.sh
@@ -1,3 +1,2 @@
-docker run --name portainer -d -p 9000:9000 -v 
-/var/run/docker.sock:/var/run/docker.sock 
-portainer/portainer
+#!/bin/bash
+docker run --name portainer -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
