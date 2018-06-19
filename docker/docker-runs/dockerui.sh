f217ab9fedaefcab25006a15d807ff8df14f70da
Rick van Lieshout <info@rickvanlieshout.com>
2018-06-19 12:08:52 +0200

    removed unnecessary files

--- docker/docker-runs/dockerui.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-docker run -d -p 9000:9000 -name docker-ui --privileged -v /var/run/docker.sock:/var/run/docker.sock uifd/ui-for-docker
--- docker/docker-runs/portainer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --name portainer -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
